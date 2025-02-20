9be98a4929081aed58256e3ab277da32095dfb58
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-02-20 16:45:58 +0530

    Update gfg-Find median in a stream.cpp

--- Day-5/potd/gfg-Find median in a stream.cpp
+++ Day-5/potd/gfg-Find median in a stream.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+  public:
+    vector<double> getMedian(vector<int> &arr) {
+        // code here
+        vector<double>ans;
+        multimap<int,int>x;
+        int n = arr.size();
+        x.insert({arr[0], 1});
+        auto it = x.begin();
+        ans.push_back(arr[0]);
+        for(int i=1; i<n; i++){
+            x.insert({arr[i], 1});
+            if(i%2 != 0){
+                if(it->first > arr[i]) it--;
+                double digit = it->first;
+                it++;
+                digit+=it->first;
+                it--;
+                ans.push_back(digit/2);
+            }
+            else{
+                if(it->first <= arr[i])  it++;
+                ans.push_back(it->first);
+            }
+        }
+        return ans;
+    }
+};
