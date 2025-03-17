13638e7f8a84a5d7fb80f5589031f0ef03806792
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-03-17 19:22:26 +0530

    Create gfg-Subset Sum Problem.cpp

---
+++ Day-29/potd/gfg-Subset Sum Problem.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n=arr.size();
+        vector<bool> prev(sum+1,false);
+        vector<bool> curr(sum+1,false);
+        prev[0]=true;
+        for(int ind=1;ind<=n;ind++){
+            for(int s=0;s<=sum;s++){
+                if(s==0){
+                    curr[s]=true;
+                    continue;
+                }
+                bool notTake=prev[s];
+                bool take=false;
+                if(s>=arr[ind-1]) take=prev[s-arr[ind-1]];
+                bool ans=take|notTake;
+                curr[s]=ans;
+            }
+            prev=curr;
+        }
+        return prev[sum];
+    }
+};
