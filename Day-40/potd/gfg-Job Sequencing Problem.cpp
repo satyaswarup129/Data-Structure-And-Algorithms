bfa170e5d0a247957d6a941e1b01d2fda4f4433b
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-03-29 20:48:32 +0530

    Create gfg-Job Sequencing Problem.cpp

---
+++ Day-40/potd/gfg-Job Sequencing Problem.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+    bool static comp(pair<int, int>&a, pair<int, int>&b){
+        return a.first>b.first;
+    }
+  
+    vector<int> jobSequencing(vector<int> &deadline, 
+    vector<int> &profit) {
+        // code here
+        int n=profit.size();
+        vector<pair<int, int>>v;
+        
+        for(int i=0; i<n; i++){
+            v.push_back({profit[i], deadline[i]});
+        }
+        
+        sort(v.begin(), v.end(), comp);
+        
+        vector<int>tmp(n+1, -1);
+        
+        int ans=0, cnt=0;
+        
+        for(int i=0; i<n; i++){
+            int ind=v[i].second;
+            while(ind>=1 && tmp[ind]>=0){
+                ind--;
+            }
+            if(ind>=1 && tmp[ind]<0){
+                cnt++;
+                ans+=v[i].first;
+                tmp[ind]=v[i].second;
+            }
+        }
+        return {cnt, ans};
+    }
+};
