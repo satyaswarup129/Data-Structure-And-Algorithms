311cbe4075446f5aeda8c05c16ec65a960d2d428
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-04-21 16:24:16 +0530

    Create gfg-Missing in Array.cpp

---
+++ Day-63/potd/gfg-Missing in Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            if(arr[i] != i+1) return i+1;
+        }
+        return arr.size()+1;
+    }
+};
