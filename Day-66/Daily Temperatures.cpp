ee067577b7531e31d107dc84b24dff39b97c46e0
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-04-24 23:46:02 +0530

    Create Daily Temperatures.cpp

---
+++ Day-66/Daily Temperatures.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; --i) {
+            while (!st.empty() && temperatures[i] >= temperatures[st.top()]) {
+                st.pop();
+            }
+            ans[i] = st.empty() ? 0 : st.top() - i;
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
