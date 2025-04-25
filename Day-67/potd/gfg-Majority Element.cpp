7084de3663cc42cf3db7c02ac000f3ad592370ba
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-04-25 21:40:34 +0530

    Create gfg-Majority Element.cpp

---
+++ Day-67/potd/gfg-Majority Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+        // code here
+        unordered_map<int,int> mpp;
+        
+        for(auto val:arr){
+            mpp[val]++;
+        }
+        
+        for(auto it:mpp){
+            if(it.second>arr.size()/2) return it.first;
+        }
+        return -1;
+    }
+};
