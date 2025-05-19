2dd43f03e248fc3a4822775031157d883b0648aa
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-05-19 21:00:45 +0530

    Create Count Good Nodes in Binary Tree.cpp

---
+++ Day-88/Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+        int goodNodes(TreeNode* r, int ma = -10000) {
+        return r ? goodNodes(r->left, max(ma, r->val)) + goodNodes(r->right, max(ma, r->val)) + (r->val >= ma) : 0;
+    }
+};
