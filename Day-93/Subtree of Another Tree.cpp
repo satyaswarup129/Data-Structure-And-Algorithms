16f0e9925bc4ae604971a75f2a1795d5085384d6
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-05-24 22:15:30 +0530

    Create Subtree of Another Tree.cpp

---
+++ Day-93/Subtree of Another Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root) return false;
+
+        if (isSameTree(root, subRoot)) return true;
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+
+    bool isSameTree(TreeNode* s, TreeNode* t) {
+        if (!s && !t) return true;
+        if (!s || !t || s->val != t->val) return false;
+
+        return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
+    }
+};
