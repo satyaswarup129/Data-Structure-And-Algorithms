ebd2cf6d6b4e0edd443f58d40b8b90bc454bfd02
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2025-07-17 21:11:47 +0530

    Update Network Delay Time.cpp

--- Day-144/Network Delay Time.cpp
+++ Day-144/Network Delay Time.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
-    int maximumLength(vector<int>& nums, int k) {
-        vector<vector<int>> dp(k, vector<int>(k, 0));
-        int ans = 0;
-        for (int num : nums) {
-            int r = num % k;
-            for (int j = 0; j < k; ++j) {
-                dp[r][j] = dp[j][r] + 1;
-                ans = max(ans, dp[r][j]);
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto& t : times) {
+            adj[t[0]].push_back({t[1], t[2]});  // from -> (to, time)
+        }
+
+        // Min-heap: (time, node)
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        vector<int> dist(n + 1, INT_MAX);
+        dist[k] = 0;
+        pq.push({0, k});
+
+        while (!pq.empty()) {
+            auto [time, node] = pq.top();
+            pq.pop();
+
+            for (auto& [nei, wt] : adj[node]) {
+                if (dist[nei] > time + wt) {
+                    dist[nei] = time + wt;
+                    pq.push({dist[nei], nei});
+                }
             }
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (dist[i] == INT_MAX) return -1;  // unreachable node
+            ans = max(ans, dist[i]);
         }
         return ans;
     }
